6cff7fe7aa32f42c545168f04cd4452274e11167
佳大 和田 <k023g0069@g.neec.ac.jp>
2024-04-13 14:47:27 +0900

    行列m1,m2の定義位置を修正

--- main.cpp
+++ main.cpp
@@ -203,6 +203,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
+
+	Matrix4x4 m1 = {
+		3.2f, 0.7f, 9.6f, 4.4f,
+		5.5f, 1.3f, 7.8f, 2.1f,
+		6.9f, 8.0f, 2.6f, 1.0f,
+		0.5f, 7.2f, 5.1f, 3.3f
+	};
+
+	Matrix4x4 m2 = {
+		4.1f, 6.5f, 3.3f, 2.2f,
+		8.8f, 0.6f, 9.9f, 7.7f,
+		1.1f, 5.5f, 6.6f, 0.0f,
+		3.3f, 9.9f, 8.8f, 2.2f
+	};
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -212,27 +227,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		Matrix4x4 m1 = {
-			3.2f, 0.7f, 9.6f, 4.4f,
-			5.5f, 1.3f, 7.8f, 2.1f,
-			6.9f, 8.0f, 2.6f, 1.0f,
-			0.5f, 7.2f, 5.1f, 3.3f
-		};
-
-
-		Matrix4x4 m2 = {
-			4.1f, 6.5f, 3.3f, 2.2f,
-			8.8f, 0.6f, 9.9f, 7.7f,
-			1.1f, 5.5f, 6.6f, 0.0f,
-			3.3f, 9.9f, 8.8f, 2.2f
-		};
-
 
 		///
 		/// ↓更新処理ここから
 		///
-
-
 
 		Matrix4x4 resultAdd = Add(m1, m2);
 		Matrix4x4 resultSubtract = Subtract(m1, m2);
@@ -256,8 +254,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply, "Multiply");
 		MatrixScreenPrintf(0, kRowHeight * 5 * 3, inverseM1, "inverseM1");
 		MatrixScreenPrintf(0, kRowHeight * 5 * 4, inverseM2, "inverseM2");
-		MatrixScreenPrintf(kColumnWidth * 5, 0,transposeM1, "transposeM1");
-		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, transposeM2,"transposeM2");
+		MatrixScreenPrintf(kColumnWidth * 5, 0, transposeM1, "transposeM1");
+		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, transposeM2, "transposeM2");
 		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5 * 2, identity, "identity");
 
 		///
